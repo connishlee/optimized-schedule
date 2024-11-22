deaba2aca4f38444a8b2166bb4fa3aae12c654b7
Connor Lee <connorlee976@gmail.com>
2024-11-22 15:08:35 -0500

    created responsive navbar

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -2,14 +2,26 @@
 
 import { useState } from "react";
 
+import HamburgerIcon from "../helpers/hamburgerIcon";
+
 const Navbar = () => {
   const [open, setOpen] = useState(false);
 
   return (
     <div className="flex">
-      <div className={`${open ? "w-72" : "w-20"} h-screen bg-slate-800`}>
-        Sidebar
+      <div
+        className={`${
+          open ? "w-64" : "w-20"
+        } transition-all ease-in-out h-screen bg-slate-800`}
+      >
+        <div onClick={() => setOpen(!open)}>
+          <HamburgerIcon
+            open={open}
+            onClick={() => setOpen(!open)}
+          />
+        </div>
       </div>
+
       <div className="p-7 text-2xl font-semibold flex-1 h-screen">
         <h1>Home page</h1>
       </div>
